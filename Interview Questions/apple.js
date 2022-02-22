bec04e865bea144933746e20a9708e4e158536c0
Bhaskar <bhaskar.sai12@gmail.com>
2022-02-22 21:30:21 +0530

    interview questions

---
+++ Interview Questions/apple.js
@@ -0,0 +1,24 @@
+//Apple Interview Quesions
+
+//1.Composting Layer CSS3
+
+//2.Past experiences in future role
+
+//3.Array flatten
+
+//4.Array map
+
+//5.Concatenate
+
+//6. Find the position of first duplicate element in a string.
+
+//7. Concatenate two array and find the unique element from it.
+
+//8. Input array [3,4,5] output array [20, 15,12]
+
+//9. What is promise, how can you execute array of promise sequence by sequence- use generator concept
+
+
+//10. How to build a npm package
+
+//11.serialize deserializer
